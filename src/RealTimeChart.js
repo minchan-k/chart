fba479f8b36197fa8bb0f5f1975ef3366c59556a
KimMinchan95 <hialscks@gmail.com>
2021-12-03 21:33:04 +0900

    reChart

--- src/RealTimeChart.js
+++ src/RealTimeChart.js
@@ -11,6 +11,13 @@ import {
     YAxis,
     ResponsiveContainer
 } from "recharts";
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 50vh;
+    width: 100vw;
+    font-weight: bold;
+`;
 
 const socket = io('http://localhost:3000', {
     transports: ['websocket', 'polling']
@@ -19,7 +26,7 @@ const socket = io('http://localhost:3000', {
 const RealTimeChart = () => {
     const arr = [];
     for (let i = 0; i < 119; i++) {
-        arr.push({name: 'no signal', value: 0});
+        arr.push({name: 'loading', value: 0});
     }
     const [data, setData] = useState(arr);
     // 1. listen for a cpu event and update the state
@@ -30,21 +37,20 @@ const RealTimeChart = () => {
     }, []);
     // 2. render the line chart using the state
     return (
-        <>
-            <h1>Real Time CPU Usage</h1>
-            <ResponsiveContainer width={1000} height={500} >
+        <Container>
+            <ResponsiveContainer width={"100%"} height={"100%"} >
                 <LineChart data={data}
                            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                    <XAxis dataKey="name" angle={5} />
+                    <XAxis dataKey="name" angle={5} minTickGap={10} />
                     <YAxis />
                     <CartesianGrid strokeDasharray="3"/>
                     <Legend align="left" verticalAlign='middle'
                             layout="vertical"
-                            margin={{right: 40}}/>
+                    />
                     <Line type="monotone" dataKey="value" stroke="#8884d8" />
                 </LineChart>
             </ResponsiveContainer>
-        </>
+        </Container>
     )
 }
 
