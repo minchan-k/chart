b52a7aa0a9fae02c087772ca85d25f54934dc6e1
KimMinchan95 <hialscks@gmail.com>
2021-12-07 23:42:43 +0900

    chart.js

--- src/chartjs.js
+++ src/chartjs.js
@@ -2,47 +2,50 @@ import { Line, Chart } from 'react-chartjs-2';
 import 'chartjs-adapter-luxon';
 import StreamingPlugin from 'chartjs-plugin-streaming';
 
+// chart와 streaming-plugin과 연결
 Chart.register(StreamingPlugin);
 
 const DoughnutChart = () => {
+    const data = {
+        datasets: [{
+            label: 'Dataset 1',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderDash: [8, 4],
+            fill: false,
+            data: []
+        }, {
+            label: 'Dataset 2',
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgb(54, 162, 235)',
+            cubicInterpolationMode: 'monotone',
+            fill: false,
+            data: []
+        }]
+    }
+    const options = {
+        scales: {
+            x: {
+                type: 'realtime',
+                realtime: {
+                    delay: 2000,
+                    onRefresh: chart => {
+                        chart.data.datasets.forEach(dataset => {
+                            dataset.data.push({
+                                x: Date.now(),
+                                y: Math.random()
+                            });
+                        });
+                    }
+                }
+            }
+        }
+    }
     return (
         <>
             <Line
-                data={{
-                    datasets: [{
-                        label: 'Dataset 1',
-                        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        borderDash: [8, 4],
-                        fill: true,
-                        data: []
-                    }, {
-                        label: 'Dataset 2',
-                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                        borderColor: 'rgb(54, 162, 235)',
-                        cubicInterpolationMode: 'monotone',
-                        fill: true,
-                        data: []
-                    }]
-                }}
-                options={{
-                    scales: {
-                        x: {
-                            type: 'realtime',
-                            realtime: {
-                                delay: 2000,
-                                onRefresh: chart => {
-                                    chart.data.datasets.forEach(dataset => {
-                                        dataset.data.push({
-                                            x: Date.now(),
-                                            y: Math.random()
-                                        });
-                                    });
-                                }
-                            }
-                        }
-                    }
-                }}
+                data={data}
+                options={options}
             />
         </>
     );
